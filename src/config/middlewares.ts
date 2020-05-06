421a0674f9f871e36f9f25b0dd5f2bc1bcf5ac61
Matheeus <you@example.com>
2020-05-05 21:01:13 -0300

    fix log

--- src/config/middlewares.ts
+++ src/config/middlewares.ts
@@ -3,14 +3,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
-import passport from './passport';
+import 'dotenv/config';
+// import passport from './passport';
 
 const Middlewares = (app: express.Application) => {
   app.use(bodyParser.json());
   app.use(cors({ origin: '*' }));
   // app.use(passport.authenticate());
-  app.use(express.static(path.resolve('./apidoc')));
-  app.use(express.static(path.resolve('./__tests__/coverage/lcov-report')));
+  // app.use(express.static(path.resolve('./apidoc')));
+  app.use(express.static(path.resolve('./_tests_/coverage/lcov-report')));
+  app.use((req, res, next) => {
+    const objeto_req = {
+      headers: req.headers
+    };
+    process.env.HEADERS_GLOBAIS = JSON.stringify(objeto_req);
+    return next();
+  });
 };
 
 export default Middlewares;
